04af9b47723cd51f80619250ebdc1806b7ee570a
lauragomidee <49699245+lauragomidee@users.noreply.github.com>
2019-04-24 20:05:41 -0300

    maiusculo.js

---
+++ fase02/ex04/maiusculo.js
@@ -0,0 +1,31 @@
+function maiusculo(palavra) {
+   var flor = palavra.substring(0,3)
+   var lápis = palavra.substring(3,palavra.length)
+  
+    return (flor.toUpperCase()+ lápis)
+}
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+

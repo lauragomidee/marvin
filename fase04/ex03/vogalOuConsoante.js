e44e582784d48737f410eab96d995f7e8479acd4
lauragomidee <49699245+lauragomidee@users.noreply.github.com>
2019-05-21 20:16:25 -0300

    vogalOuConsoante.js

--- fase04/ex03/vogalOuConsoante.js
+++ fase04/ex03/vogalOuConsoante.js
@@ -1,13 +1,9 @@
 function vogalOuConsoante(letra){
     if (letra === 'A'||letra ==='E'||letra ==='I'||letra ==='O'||letra ==='U'
-    ||letra ==='a'||letra ==='e'||letra === 'i'||letra ==='o'||letra === 'u') {
-    return 'vogal'
-    }else{
-        return 'consoante'
+    ||letra ==='a'||letra ==='e'||letra === 'i'||letra ==='o'||letra === 'u') 
+    return 'Vogal'
+    else
+        return 'Consoante'
     }
 
-    
-
-
-    
 

c382632cf450e5df15eca58994ad934f6532d8a0
lauragomidee <49699245+lauragomidee@users.noreply.github.com>
2019-05-21 20:11:01 -0300

    nove.js

--- fase04/ex01/nove.js
+++ fase04/ex01/nove.js
@@ -1,10 +1,13 @@
-function nove(num) {
-    if (num % 10 ===9) {
-        return 'true'
-    }else{
-        return 'false'
-    }
+    
+function nove(y) {
+ 
+ var x = y.toString()
+ 
+  var t = x.charAt(x.length - 1)
+  
+  if (t == 9) {return true}
+  else {return false}
+
 }
 
-    
 

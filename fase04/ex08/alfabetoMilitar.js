41a098ff8c886e1c2b4176c9a754faa70415a4e4
lauragomidee <49699245+lauragomidee@users.noreply.github.com>
2019-05-21 19:43:56 -0300

    alfabetoMilitar.js

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,17 @@
+function alfabetoMilitar(vogal){
+    if (vogal === 'a')
+    return 'alpha' 
+    if (vogal === 'e')
+    return 'echo'
+    if (vogal === 'i')
+    return 'india'
+    if (vogal === 'o')
+    return 'oscar'
+    if (vogal ==='u')
+    return 'uniform'
+    else
+    return 'false'
+}
+
+    
+

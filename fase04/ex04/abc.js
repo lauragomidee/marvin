5027dd2829919aadee510ef655ba68c77215a0ff
lauragomidee <49699245+lauragomidee@users.noreply.github.com>
2019-05-21 20:20:29 -0300

    abc.js

--- fase04/ex04/abc.js
+++ fase04/ex04/abc.js
@@ -1,15 +1,11 @@
+    
 function abc(letra){
     if(letra.substring(0,1) === 'a'||letra.substring(0,1) === 'b'||letra.substring(0,1) === 'c'
-    ||letra.substring(0,1) === 'A'||letra.substring(0,1) === 'B'||letra.substring(0,1) === 'C') {
-        return 'true'
-    }else{
-        return 'false'
+    ||letra.substring(0,1) === 'A'||letra.substring(0,1) === 'B'||letra.substring(0,1) === 'C') 
+        return true
+    else
+        return false
     }
-}
 
 
-    
-
-
-    
 

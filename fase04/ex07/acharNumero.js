528894ad0f64ff7c15891d3fb58bbbddca2de66d
lauragomidee <49699245+lauragomidee@users.noreply.github.com>
2019-05-21 19:27:05 -0300

    acharNumero.js

---
+++ fase04/ex07/acharNumero.js
@@ -0,0 +1,11 @@
+function acharNumero(x) {
+    if (x < 5)
+    return 'a'
+    if (x >= 5 && x <= 10)
+    return 'b'
+    if (x >= 10 && x <= 100)
+    return 'c'
+    if (x > 100)
+    return 'd'
+}
+
